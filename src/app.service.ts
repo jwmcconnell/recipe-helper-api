a851e27bcca989cc22a2176d583b2d883bc933cd
Jack McConnell <jackmc1122@gmail.com>
2024-08-15 18:18:58 -0400

    Add initial openai connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  async getHello(): Promise<string> {
+    const client = new OpenAI({ apiKey: process.env['OPENAI_API_KEY'] });
+    const chatCompletion = await client.chat.completions.create({
+      messages: [{ role: 'user', content: 'Say this is a test' }],
+      model: 'gpt-4o-mini',
+    });
+    console.log(JSON.stringify(chatCompletion, null, 2));
     return 'Hello World!';
   }
 }
