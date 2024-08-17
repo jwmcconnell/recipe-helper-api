476dbf94af1bcc13d4113b454f6bbfec50b60673
Jack McConnell <jackmc1122@gmail.com>
2024-08-17 12:37:59 -0400

    Install fly and remove chatgpt for the moment to prevent abuse

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import OpenAI from 'openai';
+// import OpenAI from 'openai';
 
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    const client = new OpenAI({ apiKey: process.env['OPENAI_API_KEY'] });
-    const chatCompletion = await client.chat.completions.create({
-      messages: [{ role: 'user', content: 'Say this is a test' }],
-      model: 'gpt-4o-mini',
-    });
-    console.log(JSON.stringify(chatCompletion, null, 2));
+    // const client = new OpenAI({ apiKey: process.env['OPENAI_API_KEY'] });
+    // const chatCompletion = await client.chat.completions.create({
+    //   messages: [{ role: 'user', content: 'Say this is a test' }],
+    //   model: 'gpt-4o-mini',
+    // });
+    // console.log(JSON.stringify(chatCompletion, null, 2));
     return 'Hello World!';
   }
 }
